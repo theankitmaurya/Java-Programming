e67fa5bd7958a7fd556d9971f7903d54b16e6035
Ankit <ankitmaurya20223@gmail.com>
2025-03-25 10:27:43 +0530

    Data_Types_Of_Expressions_And_Increment_And_Decrement_Operators.java Completed

---
+++ Java/src/Data_Types_Of_Expressions_And_Increment_And_Decrement_Operators.java
@@ -0,0 +1,57 @@
+
+public class Data_Types_Of_Expressions_And_Increment_And_Decrement_Operators {
+
+    public static void main(String[] args) {
+        //   Following table summarizes the resulting data types after arithmetic operation on them
+
+        byte a = 45;
+        short b = 12;
+        int c = 23;
+        double d = 67.2D;
+        float e = 27.4F;
+        long f = 123456789L;
+
+        // Result = Byte + Short = Int
+        System.out.println("Result of Byte + Short: " + (a + b));
+
+        // Result = Short + Int = Int
+        System.out.println("Result of Short + Int: " + (b + c));
+
+        // Result = Long + Float = Float
+        System.out.println("Result of Long + Float: " + (f + e));
+
+        // Result = Int + Float = Float
+        System.out.println("Result of Int + Float: " + (c + e));
+
+        // Result = Int + Int = Int
+        System.out.println("Result of Int + Int: " + (c + c));
+
+        // Result = Int + Short = Int
+        System.out.println("Result of Int + Short: " + (c + b));
+
+        // Result = Long + Double = Double
+        System.out.println("Result of Long + Double: " + (f + d));
+
+        // Result = Float + Double = Double
+        System.out.println("Result of Float + Double: " + (e + d));
+
+        // Result = Byte + Float = Float
+        System.out.println("Result of Byte + Float: " + (a + e));
+        System.out.println();
+        System.out.println("Increment And Decrement Operators ->");
+
+//        Increment And Decrement Operators
+        int g = 3;
+        System.out.println(g);
+        System.out.println(g++);
+        System.out.println(g);
+
+        int h = 5;
+        System.out.println(h);
+        System.out.println(++h);
+        System.out.println(h);
+
+//        a++ -> First use the value and then increment
+//        ++a -> First increment the value then use it
+    }
+}
